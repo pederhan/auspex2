77117397ebc4d77a0091662f412abeb296385b73
Peder Hovdan Andresen <pederhan@uio.no>
2022-08-05 13:15:57 +0200

    Rename PlotData -> Plot, add PlotData generic class

--- auspex2/models.py
+++ auspex2/models.py
@@ -1,7 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from enum import Enum, auto
 from pathlib import Path
-from typing import Optional
+from typing import Any, Generic, List, Optional, TypeVar
+
+T = TypeVar("T")
+S = TypeVar("S")
 
 
 class PlotType(Enum):
@@ -13,7 +16,17 @@ class PlotType(Enum):
 
 
 @dataclass
-class PlotData:
+class PlotData(Generic[T, S]):
+    labels: List[T] = field(default_factory=list)
+    values: List[S] = field(default_factory=list)
+
+    def __post_init__(self):
+        if len(self.labels) != len(self.values):
+            raise ValueError("labels and values must be the same length")
+
+
+@dataclass
+class Plot:
     title: str
     plot_type: PlotType
     description: str = ""
