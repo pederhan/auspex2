6feb855acab80417587188684c8f61eacce35c22
Peder Hovdan Andresen <pederhan@uio.no>
2022-08-09 13:55:12 +0200

    Add min, max npmath functions

--- auspex2/npmath.py
+++ auspex2/npmath.py
@@ -1,9 +1,12 @@
-from typing import Any, Callable, Iterable
+from typing import Any, Callable, Iterable, Union
 
 import numpy as np
 from loguru import logger
 
 from .types import NumberType
+
+_min = min
+_max = max
 
 
 def mean(a: Iterable[NumberType]) -> float:
@@ -18,6 +21,14 @@ def stdev(a: Iterable[NumberType]) -> float:
     return _do_stats_math(np.std, a)
 
 
+def min(a: Iterable[Union[int, float]]) -> float:  # todo : fix type
+    return _min(a, default=0.0)
+
+
+def max(a: Iterable[Union[int, float]]) -> float:  # todo : fix type
+    return _max(a, default=0.0)
+
+
 def _do_stats_math(
     func: Callable[[Any], np.number[Any]],
     a: Iterable[NumberType],
@@ -27,7 +38,7 @@ def _do_stats_math(
     try:
         res = func(a)
         if np.isnan(res):
-            logger.warning(
+            logger.debug(
                 f"{func.__name__}({repr(a)}) returned nan. Defaulting to {default}"
             )
             return default
