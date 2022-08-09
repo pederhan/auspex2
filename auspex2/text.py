87b94500c06b573f7407cbf1a2a81b024096685c
Peder Hovdan Andresen <pederhan@uio.no>
2022-08-09 13:54:56 +0200

    Add text tokens

--- auspex2/text.py
+++ auspex2/text.py
@@ -1,9 +1,47 @@
-from typing import NamedTuple
+"""This module defines tokens used to render text in output (tables, etc.)."""
+
+# TODO Future optimization:
+#       * use normal classes with __slots__
+#           Currently uses dataclasses for rapid iteration
+#       * be able to combine tokens (e.g. bold + italic)
 
 
-class Hyperlink(NamedTuple):
+from dataclasses import dataclass
+from typing import Any, NamedTuple
+
+
+@dataclass
+class Text:
+    text: Any  # define type for str, int, etc.
+
+    def render(self) -> str:
+        """Renders the text as a string."""
+        return str(self.text)
+
+    def render_html(self) -> str:
+        """Renders the text as HTML."""
+        return str(self.text)
+
+
+@dataclass
+class Hyperlink(Text):
     """Class that encapsulates a text with a corresponding URL.
     Used to render hyperlinks in the text."""
 
     url: str
-    text: str
+    # text: Text # this way we can combine tokens?
+
+    def render_html(self) -> str:
+        return f"<a href='{self.url}'>{self.text}</a>"
+
+
+@dataclass
+class Italic(Text):
+    def render_html(self) -> str:
+        return f"<i>{self.text}</i>"
+
+
+@dataclass
+class Bold(Text):
+    def render_html(self) -> str:
+        return f"<b>{self.text}</b>"
