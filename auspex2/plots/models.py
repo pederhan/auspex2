8c641ead23429a4a61e42c4d03227a61744455ab
Peder Hovdan Andresen <pederhan@uio.no>
2022-08-10 10:43:38 +0200

    Use Text for Plot and Table texts

--- auspex2/plots/models.py
+++ auspex2/plots/models.py
@@ -3,6 +3,10 @@ from enum import Enum, auto
 from pathlib import Path
 from typing import Any, Generic, List, Optional, TypeVar, Union
 
+from pydantic import BaseModel
+
+from ..text import Text, text_validator
+
 T = TypeVar("T")
 S = TypeVar("S")
 
@@ -25,13 +29,30 @@ class PlotData(Generic[T, S]):
             raise ValueError("labels and values must be the same length")
 
 
-@dataclass
-class Plot:
-    title: str
+class Plot(BaseModel):
+    title: str  # TODO: use Text?
     plot_type: PlotType
-    description: str = ""
-    caption: str = ""
+    description: Union[Text, str] = Text()
+    caption: Union[Text, str] = Text()
     path: Optional[Path] = None
+
+    class Config:
+        arbitrary_types_allowed = True
+        validate_assignment = True
+
+    # @validator("description")
+    # def coerce_description(cls, v: Any) -> Union[Text, str]:
+    #     if isinstance(v, str):
+    #         return Text(v)
+    #     return v
+
+    _description_validator = text_validator("description")
+
+    def __post_init__(self) -> None:
+        if isinstance(self.description, str):
+            self.description = Text(self.description)
+        if isinstance(self.caption, str):
+            self.caption = Text(self.caption)
 
 
 class PieChartStyle(Enum):
--- auspex2/tables/models.py
+++ auspex2/tables/models.py
@@ -1,19 +1,28 @@
 from dataclasses import dataclass, field
 from typing import Any
 
-from ..text import Text
+from ..text import Text, text_validator
 
 
 @dataclass
 class Table:
+    # TODO: make use of Text
     title: str
     header: list[str] = field(default_factory=list)  # column names
     rows: list[list[Text]] = field(
         default_factory=list
     )  # each row is a list of len(header)
-    caption: str = ""
-    description: str = ""
+    caption: Text = Text()
+    description: Text = Text()
     # TODO: add rich description class
+
+    _header_validator = text_validator("header")
+    _caption_validator = text_validator("caption")
+    _description_validator = text_validator("description")
+
+    class Config:
+        arbitrary_types_allowed = True
+        validate_assignment = True
 
     def __post_init__(self):
         if self.rows and len(self.header) != len(self.rows[0]):
