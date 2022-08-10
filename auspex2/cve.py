624172851c93cbc2c8407fcbfca48e2c2162f69a
Peder Hovdan Andresen <pederhan@uio.no>
2022-08-10 15:02:12 +0200

    Add sort_distribution()

--- auspex2/cve.py
+++ auspex2/cve.py
@@ -1,7 +1,17 @@
-from typing import Iterable
+from collections import Counter
+from typing import Iterable, List, Tuple
 
 from harborapi.models.scanner import Severity
 from pydantic import BaseModel
+
+CVE_PRIO = [  # low -> high
+    Severity.unknown,
+    Severity.negligible,
+    Severity.low,
+    Severity.medium,
+    Severity.high,
+    Severity.critical,
+]
 
 
 class CVSS(BaseModel):
@@ -16,18 +26,19 @@ class CVSS(BaseModel):
 
 def most_severe(severities: Iterable[Severity]) -> Severity:
     """Returns the highest severity in a list of severities."""
-    prio = [  # low -> high
-        Severity.unknown,
-        Severity.negligible,
-        Severity.low,
-        Severity.medium,
-        Severity.high,
-        Severity.critical,
-    ]
+
     # TODO: add test to ensure we test every possible Severity value
     highest_idx = 0
     for s in severities:
-        i = prio.index(s)
+        i = CVE_PRIO.index(s)
         if i > highest_idx:
             highest_idx = i
-    return prio[highest_idx]
+    return CVE_PRIO[highest_idx]
+
+
+def sort_distribution(distribution: Counter[Severity]) -> List[Tuple[Severity, int]]:
+    """Sort the distribution of severities by severity."""
+    return [
+        (k, v)
+        for k, v in sorted(distribution.items(), key=lambda x: CVE_PRIO.index(x[0]))
+    ]
